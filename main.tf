2ebd3315ef908a369d482a9f7c3a517e1d1c69b0
mallika-techie <gonelamallika@gmail.com>
2024-09-03 18:12:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,7 @@
 resource "local_file" "my_pet" {
     filename = "/pets.txt"
     content = "My cat name is TOM"
-    lifecycle {
-      prevent_destroy = true
-    }
+    
 }
 
 resource "random_pet" "petname" {
